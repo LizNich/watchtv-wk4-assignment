df55bdc6af32c6f011b4cacf19730f83de49d6b2
LizNich <nicholsonliz@hotmail.com>
2024-10-26 17:34:10 +0100

    Linked to Render

--- client/app.js
+++ client/app.js
@@ -1,9 +1,13 @@
 const watchtvContainer = document.getElementById("watchtv-container");
 
 async function getWatchtv() {
-  const response = await fetch("http://localhost:8080");
+  const response = await fetch(
+    "https://watchtv-wk4-assignment-client.onrender.com/"
+  );
   const watchtv = await response.json();
   //console.log(watchtv);  it gave errors here - connection via row 4
+
+  //
   for (let i = 0; i < watchtv.length; i++) {
     const username = watchtv[i].username;
     const message = watchtv[i].message;
