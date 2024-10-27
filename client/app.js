c5535bff4131786cce123a2e57e279bfc6a6bf54
LizNich <nicholsonliz@hotmail.com>
2024-10-27 17:39:13 +0000

    Draft submission

--- client/app.js
+++ client/app.js
@@ -1,11 +1,33 @@
 const watchtvContainer = document.getElementById("watchtv-container");
+const form = document.querySelector("form");
 
+//
+//
+async function handleSubmit(event) {
+  event.preventDefault();
+  //
+  const formData = new FormData(form);
+  const formObj = Object.fromEntries(formData);
+  //"http://localhost:8080/watchtv"
+  const response = await fetch("http://localhost:8080/watchtv", {
+    method: "POST",
+    body: JSON.stringify(formObj),
+    headers: { "Content-Type": "application/json" },
+  });
+  const data = await response.json();
+  console.log(watchtv);
+}
+
+form.addEventListener("submit", handleSubmit);
+
+//
+//
 async function getWatchtv() {
   const response = await fetch(
     "https://watchtv-wk4-assignment-client.onrender.com/"
   );
   const watchtv = await response.json();
-  //console.log(watchtv);  it gave errors here - connection via row 4
+  //console.log(watchtv);  it gave errors here - connection via row above with link on it
 
   //
   for (let i = 0; i < watchtv.length; i++) {
--- server/server.js
+++ server/server.js
@@ -21,7 +21,24 @@ app.get("/watchtv", async function (request, response) {
   const result = await db.query("SELECT * FROM watchtv");
   const watchtv = result.rows;
   response.json(watchtv);
+}); //don't touch this - it finally works
+
+// add POST
+//app.post("/watchtv", function (request, response) {
+//  response.json("POST does it work?");
+//});
+
+app.post("/watchtv", async function (request, response) {
+  const username = request.body.username;
+  const message = request.body.message;
+  const result = await db.query(
+    "INSERT INTO watchtv (username, message) VALUES ($1, $2)",
+    [username, message]
+  );
+  response.json(result);
 });
+
+//app.listen(8080, () => console.log("Running on PORT 8080"));
 
 app.listen(8080, function () {
   console.log("App is running of PORT 8080");
