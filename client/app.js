5a1c94c9481ac895ecc88371fe0ed8d559e84ddf
LizNich <nicholsonliz@hotmail.com>
2024-10-29 16:29:12 +0000

    refresh code from feedback

--- client/app.js
+++ client/app.js
@@ -7,16 +7,15 @@ async function handleSubmit(event) {
   const formData = new FormData(form);
   const formObj = Object.fromEntries(formData);
 
-  //const response = await fetch("http://localhost:8080/watchtv", {
   const response = await fetch("http://localhost:8080/watchtv", {
     method: "POST",
     body: JSON.stringify(formObj),
+    //was (data)
     headers: { "Content-Type": "application/json" },
   });
-  const data = await response.json();
-  console.log(data);
+  const reply = await response.json();
+  console.log(reply);
 }
-
 form.addEventListener("submit", handleSubmit);
 
 async function getWatchtv() {
@@ -25,12 +24,6 @@ async function getWatchtv() {
   );
   const watchtv = await response.json();
 
-  // async function getWatchtv() {
-  // const response = await fetch();
-  // ("https://watchtv-wk4-assignment-server.onrender.com/");
-  //const watchtv = await response.json();
-
-  //
   for (let i = 0; i < watchtv.length; i++) {
     const username = watchtv[i].username;
     const message = watchtv[i].message;
@@ -41,7 +34,3 @@ async function getWatchtv() {
   }
 }
 getWatchtv();
-
-//https://watchtv-wk4-assignment-server.onrender.com
-//  ("https://watchtv-wk4-assignment-client.onrender.com/");
-//("http://localhost:8080/watchtv"
--- server/server.js
+++ server/server.js
@@ -10,29 +10,28 @@ app.use(cors());
 app.use(express.json());
 dotenv.config(); // link to .env
 
-// Set up DB connection
-const db = new pg.Pool({ connectionString: process.env.DATABASE_URL }); // DB url is in the .env
+// Set up DB connection - link in the .env
+const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
-app.get("/", function (request, response) {
-  response.json("Rout Route");
-});
+app.get("/", (req, res) => res.json("Root route"));
 
-app.get("/watchtv", async function (request, response) {
+app.get("/watchtv", async function (req, res) {
   const result = await db.query("SELECT * FROM watchtv");
   const watchtv = result.rows;
+  console.log(watchtv);
   response.json(watchtv);
 }); //don't touch this - it works
 
 app.post("/watchtv", async function (request, response) {
-  const username = request.body.username;
-  const message = request.body.message;
+  const { username, message } = req.body;
   const result = await db.query(
     "INSERT INTO watchtv (username, message) VALUES ($1, $2)",
     [username, message]
   );
-  response.json(result);
+  //response.json(result);
 });
 
+// Run Server
 app.listen(8080, function () {
   console.log("App is running of PORT 8080");
 });
